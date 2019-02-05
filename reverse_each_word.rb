ec6269235dcca9cf7128a3900fe57875913c47ee
Kevin Lopez Cifuentes <us.kevin@icloud.com>
2019-02-05 21:20:43 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -9,4 +9,10 @@ def reverse_each_word(string)
 end
 
 def reverse_each_word(string)
-  
+  words_strings = string.split(" ")
+  new_strings = []
+  words_strings.collect do|i|
+    new_strings << i.reverse
+  end
+  new_strings.join(" ")
+end

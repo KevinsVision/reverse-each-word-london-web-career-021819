151ba45e4769451f807e8d03ebf0a6656d2ab9fe
Kevin Lopez Cifuentes <us.kevin@icloud.com>
2019-02-05 20:49:55 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 
 def reverse_each_word(string)
   new_string = ""
-  string.length = self.length - 1
+  string_length = self.length - 1
   new_string.downto(0) do |i|
     new_string << self|i|
   end
